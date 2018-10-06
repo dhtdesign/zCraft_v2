a3b5e44dee50ccb67a4b57d0a932f1ea055f9a16
DHT <dht.design@gmail.com>
2018-10-06 22:15:53 +0200

    update

--- java/com/dhtdesign/zcraftmod/init/ModRecipes.java
+++ java/com/dhtdesign/zcraftmod/init/ModRecipes.java
@@ -1,12 +1,12 @@
 package com.dhtdesign.zcraftmod.init;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 	
+
 	public static void init()
 	{
 		//Smelting
@@ -14,7 +14,7 @@ public class ModRecipes {
 		//GameRegistry.addSmelting(ModBlocks.BLOCK_ENDER, new ItemStack(Blocks.DIAMOND_BLOCK, 64), 3.5F);
 		GameRegistry.addSmelting(ModItems.DUST_IRON, new ItemStack(Items.IRON_INGOT, 1), 0.8F);
 		GameRegistry.addSmelting(ModItems.ENDER_DUST, new ItemStack(ModItems.INGOT_ENDER, 1), 0.8F);
-		
+		GameRegistry.addSmelting(ModBlocks.ORE_OVERWORLD, new ItemStack(ModItems.INGOT_AZOPROIT, 1), 0.8F);
 		//Potions
 		
 	}
